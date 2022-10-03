52abb476d971a9f3c8fb5358d0232d9519e40263
rkanand007 <ramkrishananand7@gmail.com>
2022-10-03 12:55:54 +0530

    added styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Board from './components/Board';
 
+import './styles/root.scss';
+
 const App = () => {
   return (
-    <div>
-      <h1>Tic Tac Toe</h1>
+    <div className="app">
+      <h1>TIC TAC TOE</h1>
       <Board />
     </div>
   );
 };
+
 export default App;
